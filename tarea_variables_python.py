cfa215540afaa4512428e86f618bfd739bc3a04a
Xavier Castro <xeduardocastro@icloud.com>
2023-07-26 18:24:49 -0600

    Primer commit

---
+++ tarea_variables_python.py
@@ -0,0 +1,18 @@
+# Declaración de variables de diferentes tipos de datos
+numero_entero = 42
+numero_flotante = 3.14
+cadena_texto = "Hola, soy una cadena"
+es_verdadero = True
+
+resultado_concatenacion = cadena_texto + " - Número entero: " + str(numero_entero) + " - Número flotante: " + str(numero_flotante) + " - Valor booleano: " + str(es_verdadero)
+
+
+
+#  suma de los primeros n números pares es igual a n * (n + 1).
+
+n = 10
+suma_numeros_pares = n * (n + 1)
+
+# Se imprimen los resultados obtenidos
+print("Resultado de la concatenación:", resultado_concatenacion)
+print("Suma de los primeros", n, "números pares:", suma_numeros_pares)
